0a2a792d458b5ff5181a44b48f06b0b387b18bb0
luiz lorenzo <luizf.lorenzo@gmail.com>
2026-05-25 23:11:08 -0300

    feat(classes-poo): cria classes pai ItemTrabalho e filha Tarefa com encapsulamento e heranca

---
+++ models/tarefa.dart
@@ -0,0 +1,30 @@
+import 'itemTrabalho.dart';
+
+class Tarefa extends ItemTrabalho {
+  final String? _responsavel;
+  final String? _status; 
+  final String? _prioridade;
+  final double? _valor;
+  final int? _horas;
+  String? get responsavel => _responsavel;
+  String? get status => _status; 
+  String? get prioridade => _prioridade;
+  double? get valor => _valor;
+  int? get horas => _horas;
+  Tarefa ({
+    required int id,
+    String? titulo,
+    String? responsavel,
+    String? status,
+    String? prioridade,
+    double? valor,
+    int? horas,
+  }) : _responsavel = responsavel, _status = status, _prioridade = prioridade, _valor = valor, _horas = horas, super(id: id, titulo: titulo);
+
+  @override
+  void exibirResumo() {
+    print('ID: $id - Título: $titulo - Responsável: $responsavel - Status: $status - Prioridade: $prioridade - Valor: ${valor != null ? 'R\$ ${valor!.toStringAsFixed(2)}' : 'N/A'} - Horas: ${horas != null ? horas.toString() : 'N/A'}');
+  }
+
+}
+
